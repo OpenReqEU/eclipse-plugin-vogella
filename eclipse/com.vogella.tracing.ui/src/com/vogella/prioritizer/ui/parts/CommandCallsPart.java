81392827e8b80861d235b8b342642c32d1df053a
Simon Scholz <simon.scholz@vogella.com>
2018-05-30 07:30:15 +0200

    Added Command calls stats part to visualize atlas graphs
    Change-Id: Ib2629ded3d2587d4016656db99658e7212fa95ec
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ eclipse/com.vogella.tracing.ui/src/com/vogella/prioritizer/ui/parts/CommandCallsPart.java
@@ -0,0 +1,38 @@
+
+package com.vogella.prioritizer.ui.parts;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Composite;
+
+public class CommandCallsPart {
+
+	private Browser browser;
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		browser = new Browser(parent, SWT.NONE);
+		Instant oneWeekBefore = ZonedDateTime.now(ZoneOffset.UTC).minusMinutes(20).toInstant();
+		updateGraph(oneWeekBefore);
+	}
+
+	@Inject
+	@Optional
+	public void updateGraph(@UIEventTopic("INSTANT") Instant instant) {
+		Instant now = Instant.now();
+		Duration duration = Duration.between(instant, now);
+		browser.setUrl("http://localhost:7101/api/v1/graph?q=name,command.calls,:eq,(,commandId,),:by&s=e-"
+				+ duration.toMinutes() + "m&l=0");
+	}
+
+}
