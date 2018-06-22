e97071933c927db63547b64ef3d9d3e6090c65d6
Simon Scholz <simon.scholz@vogella.com>
2018-06-22 10:52:01 +0200

    Added action to open the keys preference page
    Change-Id: I61b2910fac8f5a9f9f52a45a021b8482f0e0dcd1
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- eclipse/com.vogella.tips/src/com/vogella/tips/ShortcutTip.java
+++ eclipse/com.vogella.tips/src/com/vogella/tips/ShortcutTip.java
@@ -3,8 +3,13 @@ package com.vogella.tips;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Collections;
 import java.util.Date;
 
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
+import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.tips.core.IHtmlTip;
 import org.eclipse.tips.core.Tip;
 import org.eclipse.tips.core.TipAction;
@@ -14,6 +19,7 @@ import org.osgi.framework.FrameworkUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("restriction")
 public class ShortcutTip extends Tip implements IHtmlTip {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ShortcutTip.class);
@@ -21,19 +27,24 @@ public class ShortcutTip extends Tip implements IHtmlTip {
 	private String commandName;
 	private String shortcut;
 
-	public ShortcutTip(String providerId, String commandName, String shortcut) {
+	public ShortcutTip(String providerId, String commandName, String shortcut, ECommandService commandService,
+			EHandlerService handlerService, UISynchronize uiSync) {
 		super(providerId);
 		this.commandName = commandName;
 		this.shortcut = shortcut;
-		Bundle bundle = FrameworkUtil.getBundle(getClass());
-		try {
-			TipImage tipImage = new TipImage(bundle.getEntry("icons/16/innosensr-logo.png"));
-			TipAction reportBugTipAction = new TipAction("Report Bug", "Create a bug report in InnoSensr.",
-					() -> System.out.println("Reporting bug"), tipImage);
-			getActions().add(reportBugTipAction);
-		} catch (IOException e) {
-			LOG.error(e.getMessage(), e);
-		}
+
+		TipAction openPreferencesAction = new TipAction("Preferences", "Show shortcuts in preferences", () -> {
+			ParameterizedCommand command = commandService.createCommand("org.eclipse.ui.window.preferences",
+					Collections.singletonMap("preferencePageId", "org.eclipse.ui.preferencePages.Keys"));
+			uiSync.asyncExec(() -> {
+				handlerService.executeHandler(command);
+			});
+		}, null);
+		TipAction reportBugTipAction = new TipAction("Report Bug", "Create a bug report in InnoSensr.",
+				() -> System.out.println("Reporting bug"), null);
+
+		getActions().add(reportBugTipAction);
+		getActions().add(openPreferencesAction);
 	}
 
 	@Override
@@ -48,9 +59,9 @@ public class ShortcutTip extends Tip implements IHtmlTip {
 
 	@Override
 	public String getHTML() {
-		return "<html><head><title>Shortcut advice</title></head><body><p>The " + commandName
-				+ " command has been used quite often.</p><p>Did you know about the " + shortcut
-				+ " shortcut to invoke this command.</p><p>Shortcuts allow you to invoke commands faster and be more efficient.</p></body></html>";
+		return "<html><head><title>Shortcut advice</title></head><body><p>The <i>" + commandName
+				+ "</i> command has been used quite often.</p><p>Did you know about the <i>" + shortcut
+				+ "</i> shortcut to invoke this command.</p><p>Shortcuts allow you to invoke commands faster and be more efficient.</p></body></html>";
 	}
 
 	@Override
--- eclipse/com.vogella.tracing.ui/src/com/vogella/prioritizer/ui/tips/CommandTracingTipProvider.java
+++ eclipse/com.vogella.tracing.ui/src/com/vogella/prioritizer/ui/tips/CommandTracingTipProvider.java
@@ -6,14 +6,19 @@ import java.util.ArrayList;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
+import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.tips.core.Tip;
 import org.eclipse.tips.core.TipImage;
 import org.eclipse.tips.core.TipProvider;
+import org.eclipse.ui.PlatformUI;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
 import com.vogella.tips.ShortcutTip;
 
+@SuppressWarnings("restriction")
 public class CommandTracingTipProvider extends TipProvider {
 
 	private TipImage tipImage;
@@ -45,14 +50,17 @@ public class CommandTracingTipProvider extends TipProvider {
 	@Override
 	public IStatus loadNewTips(IProgressMonitor monitor) {
 
+		ECommandService commandService = PlatformUI.getWorkbench().getService(ECommandService.class);
+		EHandlerService handlerService = PlatformUI.getWorkbench().getService(EHandlerService.class);
+		UISynchronize uiSync = PlatformUI.getWorkbench().getService(UISynchronize.class);
 		ArrayList<Tip> tips = new ArrayList<>();
 		tips.add(new CommandInvocationGraphTip(getID()));
 		tips.add(new CommandInvocationShortCutTip(getID()));
 		tips.add(new MenuDepthGraphTip(getID()));
-		tips.add(new ShortcutTip(getID(), "Refresh", "F5"));
-		tips.add(new ShortcutTip(getID(), "Properties", "ALT + ENTER"));
-		tips.add(new ShortcutTip(getID(), "Save", "CTRL + S"));
-		tips.add(new ShortcutTip(getID(), "Save All", "CTRL + SHIFT + S"));
+		tips.add(new ShortcutTip(getID(), "Refresh", "F5", commandService, handlerService, uiSync));
+		tips.add(new ShortcutTip(getID(), "Properties", "ALT + ENTER", commandService, handlerService, uiSync));
+		tips.add(new ShortcutTip(getID(), "Save", "CTRL + S", commandService, handlerService, uiSync));
+		tips.add(new ShortcutTip(getID(), "Save All", "CTRL + SHIFT + S", commandService, handlerService, uiSync));
 
 		setTips(tips);
 
