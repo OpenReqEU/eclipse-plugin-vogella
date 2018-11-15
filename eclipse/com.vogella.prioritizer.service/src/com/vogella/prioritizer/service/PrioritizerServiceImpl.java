23e527b89f5541623947a7b89b6352461c06f0ce
Jennifer Nerlich <jennifer.nerlich@vogella.com>
2018-11-15 09:40:24 +0100

    Change to call the service on the ENG environment

--- eclipse/com.vogella.prioritizer.service/src/com/vogella/prioritizer/service/PrioritizerServiceImpl.java
+++ eclipse/com.vogella.prioritizer.service/src/com/vogella/prioritizer/service/PrioritizerServiceImpl.java
@@ -39,7 +39,7 @@ public class PrioritizerServiceImpl implements PrioritizerService {
 
 	public class ServerSettings {
 		@Parameter(names = "-serverUrl", description = "Specify a custom server url for the prioritizer")
-		private String serverUrl = "http://129.27.202.66:9002/";
+		private String serverUrl = "http://217.172.12.199:9002/";
 
 		public String getServerUrl() {
 			return serverUrl;
