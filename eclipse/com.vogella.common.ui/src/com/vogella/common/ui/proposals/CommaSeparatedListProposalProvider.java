f9413700b4276bb78cddbc86fd2f510aa794bc9b
Simon Scholz <simon.scholz@vogella.com>
2018-12-07 17:46:26 +0100

    Added custom IContentProposalProvider for command separated lists
    Change-Id: I86e7ec801f227aa7f82abdd993f1a5a40bbaa9f5
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- eclipse/com.vogella.common.ui/src/com/vogella/common/ui/proposals/CommaSeparatedListProposalProvider.java
+++ eclipse/com.vogella.common.ui/src/com/vogella/common/ui/proposals/CommaSeparatedListProposalProvider.java
@@ -3,6 +3,7 @@ package com.vogella.common.ui.proposals;
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.jface.fieldassist.ContentProposal;
 import org.eclipse.jface.fieldassist.IContentProposal;
 import org.eclipse.jface.fieldassist.IContentProposalProvider;
 
@@ -17,10 +18,27 @@ public class CommaSeparatedListProposalProvider implements IContentProposalProvi
 	public CommaSeparatedListProposalProvider(String... proposals) {
 		this.proposals = Arrays.asList(proposals);
 	}
-	
+
 	@Override
 	public IContentProposal[] getProposals(String contents, int position) {
-		return null;
+		int lastIndexOfComma = contents.lastIndexOf(',');
+
+		if (lastIndexOfComma < 0) {
+			return filterContainsIgnoreCase(contents.trim());
+		}
+
+		if (contents.length() < lastIndexOfComma + 1) {
+			return proposals.stream().map(p -> new ContentProposal(p)).toArray(IContentProposal[]::new);
+		}
+
+		String searchString = contents.substring(lastIndexOfComma + 1).trim();
+
+		return filterContainsIgnoreCase(searchString);
+	}
+
+	private IContentProposal[] filterContainsIgnoreCase(String contents) {
+		return proposals.stream().filter(p -> p.toLowerCase().contains(contents.toLowerCase()))
+				.map(p -> new ContentProposal(p)).toArray(IContentProposal[]::new);
 	}
 
 }
--- eclipse/com.vogella.common.ui/src/com/vogella/common/ui/util/WidgetUtils.java
+++ eclipse/com.vogella.common.ui/src/com/vogella/common/ui/util/WidgetUtils.java
@@ -7,7 +7,6 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.bindings.keys.KeyStroke;
 import org.eclipse.jface.fieldassist.ContentProposalAdapter;
 import org.eclipse.jface.fieldassist.ControlDecoration;
-import org.eclipse.jface.fieldassist.SimpleContentProposalProvider;
 import org.eclipse.jface.fieldassist.TextContentAdapter;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ResourceManager;
@@ -16,51 +15,73 @@ import org.eclipse.swt.widgets.Text;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.FrameworkUtil;
 
+import com.vogella.common.ui.proposals.CommaSeparatedListProposalProvider;
+
 public class WidgetUtils {
 
-	private WidgetUtils() {}
-	
+	private WidgetUtils() {
+	}
+
 	private static ImageDescriptor smartModeImageDescriptor;
-	
+
 	static {
-        Bundle bundle = FrameworkUtil.getBundle(WidgetUtils.class);
-        URL find = FileLocator.find(bundle, new Path("/icons/smartmode.png"));
-        smartModeImageDescriptor = ImageDescriptor.createFromURL(find);
+		Bundle bundle = FrameworkUtil.getBundle(WidgetUtils.class);
+		URL find = FileLocator.find(bundle, new Path("/icons/smartmode.png"));
+		smartModeImageDescriptor = ImageDescriptor.createFromURL(find);
 	}
-	
+
 	private static final String LCL = "abcdefghijklmnopqrstuvwxyz";
-    private static final String UCL = LCL.toUpperCase();
-    private static final String NUMS = "0123456789";
-	
+	private static final String UCL = LCL.toUpperCase();
+	private static final String NUMS = "0123456789";
+
 	public static void createContentAssist(Text text, ResourceManager resourceManager, String... proposals) {
-		SimpleContentProposalProvider proposalProvider = new SimpleContentProposalProvider(proposals);
-        ContentProposalAdapter proposalAdapter = new ContentProposalAdapter(
-            text,
-            new TextContentAdapter(),
-            proposalProvider,
-            getActivationKeystroke(),
-            getAutoactivationChars());
-        proposalProvider.setFiltering(true);
-        proposalAdapter.setPropagateKeys(true);
-        proposalAdapter.setProposalAcceptanceStyle(ContentProposalAdapter.PROPOSAL_INSERT);
-        
-        ControlDecoration decoration = new ControlDecoration(text, SWT.TOP | SWT.LEFT);
-        decoration.setImage(resourceManager.createImage(smartModeImageDescriptor));
-        decoration.setDescriptionText("This text field has content assist. (CTRL + SPACE)");
-        decoration.setShowOnlyOnFocus(true);
+		CommaSeparatedListProposalProvider proposalProvider = new CommaSeparatedListProposalProvider(proposals);
+		ContentProposalAdapter proposalAdapter = new ContentProposalAdapter(text, new TextContentAdapter(),
+				proposalProvider, getActivationKeystroke(), getAutoactivationChars());
+		proposalAdapter.setPropagateKeys(true);
+		proposalAdapter.setProposalAcceptanceStyle(ContentProposalAdapter.PROPOSAL_IGNORE);
+		proposalAdapter.addContentProposalListener(proposal -> {
+			String controlContents = proposalAdapter.getControlContentAdapter().getControlContents(text);
+			int cursorPosition = proposalAdapter.getControlContentAdapter().getCursorPosition(text);
+
+			String skipProposalFilterText;
+			int lastIndexOfComma = controlContents.lastIndexOf(',');
+			if (lastIndexOfComma > -1) {
+				skipProposalFilterText = controlContents.substring(0, lastIndexOfComma);
+				lastIndexOfComma = controlContents.lastIndexOf(',');
+				if (lastIndexOfComma > -1) {
+					skipProposalFilterText = controlContents.substring(0, lastIndexOfComma);
+				}
+			} else {
+				skipProposalFilterText = controlContents.substring(cursorPosition);
+			}
+
+			StringBuilder sb = new StringBuilder(skipProposalFilterText);
+
+			if (!skipProposalFilterText.isEmpty()) {
+				sb.append(",");
+			}
+
+			String newContent = sb.append(proposal.getContent()).append(",").toString();
+
+			proposalAdapter.getControlContentAdapter().setControlContents(text, newContent, newContent.length());
+		});
+
+		ControlDecoration decoration = new ControlDecoration(text, SWT.TOP | SWT.LEFT);
+		decoration.setImage(resourceManager.createImage(smartModeImageDescriptor));
+		decoration.setDescriptionText("This text field has content assist. (CTRL + SPACE)");
+		decoration.setShowOnlyOnFocus(true);
 	}
-	
-	
-	
-    private static char[] getAutoactivationChars() {
-        // To enable content proposal on deleting a char
-        String allChars = LCL + UCL + NUMS;
-        return allChars.toCharArray();
-    }
- 
-    private static KeyStroke getActivationKeystroke() {
-        KeyStroke keyStroke = KeyStroke.getInstance(
-                SWT.CTRL, ' ');
-        return keyStroke;
-    }
+
+	private static char[] getAutoactivationChars() {
+		// To enable content proposal on deleting a char
+		String delete = new String(new char[] { 8 });
+		String allChars = LCL + UCL + NUMS + delete;
+		return allChars.toCharArray();
+	}
+
+	private static KeyStroke getActivationKeystroke() {
+		KeyStroke keyStroke = KeyStroke.getInstance(SWT.CTRL, ' ');
+		return keyStroke;
+	}
 }
