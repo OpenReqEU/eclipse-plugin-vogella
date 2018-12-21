8ede865c1e6a3f34765e1043fa4c96b270be6c5d
Simon Scholz <simon.scholz@vogella.com>
2018-12-21 19:22:51 +0100

    Added new preference key for defered bugs
    Change-Id: I85413e9c932a04faf158206e56082b456d70870b
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- eclipse/com.vogella.prioritizer.core/src/com/vogella/prioritizer/core/preferences/Preferences.java
+++ eclipse/com.vogella.prioritizer.core/src/com/vogella/prioritizer/core/preferences/Preferences.java
@@ -4,6 +4,7 @@ public interface Preferences {
 	static final String PRIORITIZER_USER_EMAIL = "vogella.prioritizer.user.email";
 	static final String PRIORITIZER_QUERY_PRODUCT = "vogella.prioritizer.query.product";
 	static final String PRIORITIZER_QUERY_COMPONENT = "vogella.prioritizer.query.component";
+	static final String PRIORITIZER_DEFER_DELAY = "vogella.prioritizer.defer.delay";
 
 	static final String MDB_QUERY_PRODUCT = "vogella.mdb.query.product";
 	static final String MDB_QUERY_COMPONENT = "vogella.mdb.query.component";
