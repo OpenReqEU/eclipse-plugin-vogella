ee78c09ace03f985cbb23402ed29c69f3ee164da
Simon Scholz <simon.scholz@vogella.com>
2019-01-04 15:25:13 +0100

    Added input about @ApiOperation and @ApiResponse for MDB
    Change-Id: I095ac32e6891d5f291641b226c529618f950fc16
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- server/com.vogella.prioritizer.server/src/main/java/com/vogella/prioritizer/rest/BugzillaController.java
+++ server/com.vogella.prioritizer.server/src/main/java/com/vogella/prioritizer/rest/BugzillaController.java
@@ -10,10 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.vogella.prioritizer.server.issue.api.model.Bug;
 import com.vogella.prioritizer.service.BugzillaService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/bugzilla")
+@Api("/bugzilla")
 class BugzillaController {
 
 	private BugzillaService prioritizerService;
@@ -23,6 +28,10 @@ class BugzillaController {
 	}
 
 	@GetMapping("/mostDiscussedBugs")
+	@ApiOperation(value = "Find most discussed bugs", notes = "Find most discussed bugs for a certain product and component. By default of the last 30 days.", response = Flux.class)
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Success", response = Bug.class)
+    })
 	public Flux<Bug> getMostDiscussedBugs(@RequestParam(name = "product", required = false, defaultValue="Platform") List<String> product,
 			@RequestParam(name = "component", required = false, defaultValue="UI") List<String> component, @RequestParam(name= "daysBack", required=false, defaultValue="30") long daysBack) {
 		return prioritizerService.getMostDiscussedBugs(product, component, daysBack);
