deef96f52389818366a5f647ca5656a4fe6aee5e
Simon Scholz <simon.scholz@vogella.com>
2019-01-04 15:24:40 +0100

    Enabled @EnableSwagger2WebFlux and configured Docket
    Change-Id: I738b7f6e7050ee0922b1e0bd52baf590da35d852
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ server/com.vogella.prioritizer.server/src/main/java/com/vogella/prioritizer/doc/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.vogella.prioritizer.doc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+@Configuration
+@EnableSwagger2WebFlux
+public class SwaggerConfig {
+
+	@Bean
+	public Docket taskApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("bugzilla-api").useDefaultResponseMessages(false)
+				.apiInfo(apiInfo()).select().paths(PathSelectors.regex("/bugzilla.*")).build();
+	}
+
+	@Bean
+	public UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder().build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Vogella bugzilla API").description("API for bugzilla issues").version("1.0")
+				.build();
+	}
+}
