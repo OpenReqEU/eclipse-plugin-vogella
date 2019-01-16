564a0afa3cab1e5b70ebf109fae0b1ba833b06ba
Simon Scholz <simon.scholz@vogella.com>
2019-01-16 18:00:14 +0100

    Updated AgentIDGenerator according to what the server needs
    Change-Id: Ic36e1904c81db6cec430e037597e2c9e71c991eb
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- eclipse/com.vogella.common.core/src/com/vogella/common/core/AgentIDGenerator.java
+++ eclipse/com.vogella.common.core/src/com/vogella/common/core/AgentIDGenerator.java
@@ -2,10 +2,9 @@ package com.vogella.common.core;
 
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Enumeration;
-
-import org.eclipse.core.runtime.Platform;
-import org.osgi.framework.Version;
 
 public class AgentIDGenerator {
 
@@ -31,12 +30,41 @@ public class AgentIDGenerator {
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
-		Version version = Platform.getBundle("org.eclipse.platform").getVersion();
-		sb.append(version.toString());
-
-		agendId = sb.toString();
+		agendId = generateAgentID(sb.toString());
 
 		return agendId;
+	}
+
+	private static String generateAgentID(String macAddress) {
+		String temp = macAddress;
+		for (int round = 0; round < 10; round++) {
+			temp = hash(temp);
+		}
+
+		/*
+		 * damit die AgentID nicht zu lange wird kürzen wir die Länge der AgentID auf 9
+		 * Zeichen, das reicht für unsere Zwecke um Kollisionen zu vermeiden
+		 */
+		return temp.substring(0, 9);
+	}
+
+	private static String hash(String input) {
+		try {
+			MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+			String salt = "PLEASE_CHANGE_SOME_RANDOM_SALT";
+			String inputWithSalt = input + salt;
+			byte[] inpBytes = inputWithSalt.getBytes();
+			byte[] inpHash = sha256.digest(inpBytes);
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < inpHash.length; i++) {
+				sb.append(Integer.toString((inpHash[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			String generatedOutput = sb.toString();
+			return generatedOutput;
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	private AgentIDGenerator() {
