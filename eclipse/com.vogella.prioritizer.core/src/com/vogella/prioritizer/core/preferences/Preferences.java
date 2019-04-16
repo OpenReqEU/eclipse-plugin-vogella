15275792c4d7bc558a2d025e8ef7691b119ca6f7
Simon Scholz <simon.scholz@vogella.com>
2019-04-16 21:40:27 +0200

    Add missing userkeywords preference constant
    Change-Id: If0f32deb6a0e8b9082a7a4cf67318f97b2e0608a
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- eclipse/com.vogella.prioritizer.core/src/com/vogella/prioritizer/core/preferences/Preferences.java
+++ eclipse/com.vogella.prioritizer.core/src/com/vogella/prioritizer/core/preferences/Preferences.java
@@ -6,6 +6,7 @@ public interface Preferences {
 	static final String PRIORITIZER_QUERY_PRODUCT = "vogella.prioritizer.query.product";
 	static final String PRIORITIZER_QUERY_COMPONENT = "vogella.prioritizer.query.component";
 	static final String PRIORITIZER_DEFER_DELAY = "vogella.prioritizer.defer.delay";
+	static final String PRIORITIZER_USER_KEYWORDS = "vogella.prioritizer.user.keywords";
 
 	static final String MDB_QUERY_PRODUCT = "vogella.mdb.query.product";
 	static final String MDB_QUERY_COMPONENT = "vogella.mdb.query.component";
